b6be0277b3a17d1ca42768726a4066f3cd0be3c2
kuining <kuiningcomeon@163.com>
2019-12-04 11:09:29 +0800

    修改监听地址

--- Program.cs
+++ Program.cs
@@ -20,7 +20,8 @@ namespace TestMvc
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    .UseUrls("http://*:5008");
                 });
     }
 }
